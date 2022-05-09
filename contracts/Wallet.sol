11390830a9d1ba717703dd1036c46bfd24740fc4
Ryan <ryan.haraki@hotmail.com>
2022-05-09 13:55:35 -0700

    smart contract sending + deposit working

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -5,18 +5,18 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Wallet {
-    function deposit() external payable {}
-
-    // Fallback function is called when msg.data is not empty
-    fallback() external payable {}
+    function deposit(uint256 _amount) external payable {
+        require(msg.value == _amount);
+        // nothing else to do
+    }
 
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    function sendEther(address payable _to) public payable {
+    function sendEther(address payable _to, uint256 _amount) public payable {
         // returns a boolean value indicating success or failure
-        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+        (bool sent, bytes memory data) = _to.call{value: _amount}("");
         require(sent, "Failed to send Ether");
     }
 }
