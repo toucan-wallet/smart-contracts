e6e5c4e332536b75d800e0a8dddc1b81a658e2ae
Ryan <ryan.haraki@hotmail.com>
2022-05-08 23:00:02 -0700

    initial commit

---
+++ contracts/Wallet.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Wallet {
+    constructor() {
+        console.log("This is my Wallet smart contract");
+    }
+}
